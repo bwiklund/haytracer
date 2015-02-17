31980301ea103acf65a986991fdf2544bfec4ada
Ben Wiklund <ben@daisyowl.com>
2015-02-16 17:17:27 -0800

    stubbing scene render stuff more

--- src/Render.hs
+++ src/Render.hs
@@ -4,10 +4,19 @@ import qualified Data.List as L
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString as B
 
+import Scene
+
 -- stub
 data RenderResult = RenderResult
 
+-- stub
+toBytes :: RenderResult -> B.ByteString
+toBytes renderResult = B.concat [B.pack [255,255,255,0,0,0,0,0,0,255,255,255]]
+
+renderScene :: Scene -> RenderResult
+renderScene scene = RenderResult
+
 toRawTest :: RenderResult -> FilePath -> IO ()
 toRawTest renderResult filePath = do
-  let bytes = B.concat [B.pack [255,255,255,0,0,0,0,0,0,255,255,255]]
+  let bytes = toBytes renderResult
   B.writeFile filePath bytes
