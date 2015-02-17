f7b48a4b85f6fd406634f368af47513408c570af
Ben Wiklund <ben@daisyowl.com>
2015-02-16 16:24:36 -0800

    scalar mult and mag

--- src/Vector.hs
+++ src/Vector.hs
@@ -8,3 +8,12 @@ add (Vector ax ay az) (Vector bx by bz) = Vector (ax+bx) (ay+by) (az+bz)
 
 sub :: Vector -> Vector -> Vector
 sub (Vector ax ay az) (Vector bx by bz) = Vector (ax-bx) (ay-by) (az-bz)
+
+mult :: Vector -> Float -> Vector
+mult (Vector x y z) n = Vector (x*n) (y*n) (z*n)
+
+mag :: Vector -> Float
+mag v = sqrt (magSq v)
+
+magSq :: Vector -> Float
+magSq (Vector x y z) = x*x + y*y + z*z
--- test/Main.hs
+++ test/Main.hs
@@ -15,7 +15,25 @@ tests = TestList [
     TestCase $ assertEqual
       "Vector subtraction"
       (sub (Vector 1 2 3) (Vector 2 4 6))
-      (Vector (-1) (-2) (-3))
+      (Vector (-1) (-2) (-3)),
+
+
+    TestCase $ assertEqual
+      "Vector scalar multiplication"
+      (mult (Vector 1 2 3) 2)
+      (Vector 2 4 6),
+
+
+    TestCase $ assertEqual
+      "Vector magnitude squared"
+      (magSq (Vector 1 2 3))
+      14,
+
+
+    TestCase $ assertEqual
+      "Vector magnitude"
+      (mag (Vector 1 2 3))
+      (sqrt 14)
 
   ]
 
