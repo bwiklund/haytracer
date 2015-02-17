034ef667f98e8a8e4b02a7bf5d81cbc2de30e5fe
Ben Wiklund <ben@daisyowl.com>
2015-02-16 18:38:46 -0800

    list comprehension camera rays

--- src/Render.hs
+++ src/Render.hs
@@ -10,15 +10,20 @@ import Vector
 data Camera = Camera {
   position :: Vector,
   direction :: Vector,
-  zoom :: Float
+  zoom :: Double
 }
 
+-- TODO: actually respect dir instead of seding out width 0 0 1 as center of screen
 cameraRaysForPlate :: Camera -> PlateSettings -> [Ray]
-cameraRaysForPlate camera plateSettings = replicate ((width plateSettings) * (height plateSettings)) (Ray (position camera) (direction camera))
+cameraRaysForPlate (Camera pos dir zoom) (PlateSettings w h) =
+  let dw = fromIntegral w
+      dh = fromIntegral h
+      rayForPixel i j = Ray pos (Vector ((i/dw - 0.5) * zoom) ((j/dh - 0.5) * zoom) 1.0)
+   in [rayForPixel i j | i <- [0.0..dw-1.0], j <- [0.0..dh-1.0]]
 
 -- move me?
 toPixel :: Ray -> [Double]
-toPixel ray = [1.0,0.0,1.0]
+toPixel (Ray (Vector x1 y1 z1) (Vector x2 y2 z2)) = [x2, y2, z2]
 
 data PlateSettings = PlateSettings { width :: Int, height :: Int }
 
@@ -34,7 +39,7 @@ toBytes plate = B.pack (map (floor . (*255)) $ buffer plate)
 renderScene :: Scene -> Camera -> PlateSettings -> Plate
 renderScene scene camera plateSettings =
   let rays = cameraRaysForPlate camera plateSettings
-   in Plate plateSettings (concat (map toPixel rays))--(BC.concat (B.pack (map toPixel rays)))
+   in Plate plateSettings (concat (map toPixel rays))
 
 toRawTest :: Plate -> FilePath -> IO ()
 toRawTest plate filePath = do
--- src/Vector.hs
+++ src/Vector.hs
@@ -1,7 +1,7 @@
 module Vector where
 
 
-data Vector = Vector Float Float Float deriving (Eq, Show)
+data Vector = Vector Double Double Double deriving (Eq, Show)
 
 add :: Vector -> Vector -> Vector
 add (Vector ax ay az) (Vector bx by bz) = Vector (ax+bx) (ay+by) (az+bz)
@@ -9,13 +9,13 @@ add (Vector ax ay az) (Vector bx by bz) = Vector (ax+bx) (ay+by) (az+bz)
 sub :: Vector -> Vector -> Vector
 sub (Vector ax ay az) (Vector bx by bz) = Vector (ax-bx) (ay-by) (az-bz)
 
-mult :: Vector -> Float -> Vector
+mult :: Vector -> Double -> Vector
 mult (Vector x y z) n = Vector (x*n) (y*n) (z*n)
 
-mag :: Vector -> Float
+mag :: Vector -> Double
 mag v = sqrt (magSq v)
 
-magSq :: Vector -> Float
+magSq :: Vector -> Double
 magSq (Vector x y z) = x*x + y*y + z*z
 
 normalize :: Vector -> Vector
